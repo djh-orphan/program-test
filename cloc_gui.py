63babefbe4a3cb0a0f3fd1d205c3f054903d154d
djh-orphan <59858543+djh-orphan@users.noreply.github.com>
2020-05-20 21:08:34 +0800

    最终版时间管理界面
    增加从数据库查询功能

--- cloc_gui.py
+++ cloc_gui.py
@@ -2,46 +2,56 @@ from tkinter import *
 import time
 import calen_gui
 import tkinter.messagebox
-
+from prettytable import PrettyTable
 import pymysql
 
 con = pymysql.connect(host='127.0.0.1', user='root', passwd='djh660993', db='data', charset='utf8')
 c = con.cursor()
 sql = "insert into event(eventCT,eventtime,eventcost) value(%s,%s,%s)"
+sql2 = "select eventCT,eventtime,eventcost from event where eventtime>=%s and eventtime<=%s"
 
 
 class ClocFrame:
     def __init__(self, master):
         self.master = master
         self.master.config()
-        self.frame2 = Frame(self.master, height=400, width=330)
+        self.frame2 = Frame(self.master, height=420, width=420)
         self.frame2.pack(fill=BOTH, expand=1)
-        self.label1 = Label(self.frame2, text="任务名称")
-        self.label1.place(x=140, y=10, height=15, width=60)
-        self.lbl1 = Label(self.frame2)
-        self.lbl1.place(x=10, y=75, height=200, width=310)
+        self.label1 = Label(self.frame2, text="任务名称:")
+        self.label1.place(x=10, y=10, height=20, width=60)
         self.work = Entry(self.frame2)
-        self.work.place(x=140, y=28, height=20, width=60)
+        self.work.place(x=80, y=10, height=20, width=270)
         self.entryWidget = Entry(self.frame2)
-        self.entryWidget.place(x=4, y=50, height=20, width=40)
+        self.entryWidget.place(x=4, y=50, height=20, width=50)
         self.a = Label(self.frame2, text="时")
-        self.a.place(x=47, y=50, height=20, width=20)
+        self.a.place(x=60, y=50, height=20, width=20)
         self.entryW = Entry(self.frame2)
-        self.entryW.place(x=70, y=50, height=20, width=40)
+        self.entryW.place(x=90, y=50, height=20, width=50)
         self.b = Label(self.frame2, text="分")
-        self.b.place(x=113, y=50, height=20, width=20)
+        self.b.place(x=150, y=50, height=20, width=20)
         self.entry = Entry(self.frame2)
-        self.entry.place(x=136, y=50, height=20, width=40)
+        self.entry.place(x=180, y=50, height=20, width=50)
         self.c = Label(self.frame2, text="秒")
-        self.c.place(x=179, y=50, height=20, width=20)
+        self.c.place(x=240, y=50, height=20, width=20)
         self.hi_there = Button(self.frame2, text="开始", command=self.start)
-        self.hi_there.place(x=201, y=50, height=20, width=40)
+        self.hi_there.place(x=270, y=50, height=20, width=40)
         self.button = Button(self.frame2, text="退出", fg="red", command=self.frame2.quit)
-        self.button.place(x=243, y=50, height=20, width=40)
+        self.button.place(x=370, y=50, height=20, width=40)
         self.button2 = Button(self.frame2, text="切换", fg="#000000", command=self.change)
-        self.button2.place(x=287, y=50, width=40, height=20)
+        self.button2.place(x=320, y=50, width=40, height=20)
+        self.entry2 = Entry(self.frame2)
+        self.entry2.place(x=10, y=90, height=20, width=130)
+        self.entry3 = Entry(self.frame2)
+        self.entry3.place(x=180, y=90, height=20, width=130)
+        self.label3 = Label(self.frame2, text="至").place(x=150, y=90, height=20, width=20)
+        self.button3 = Button(self.frame2, text="查询", command=self.check).place(x=320, y=90, height=20, width=40)
+        self.lbl1 = Label(self.frame2)
+        self.text = Text(self.frame2)
 
     def start(self):
+        self.lbl1 = Label(self.frame2)
+        self.text.destroy()
+        self.lbl1.place(x=10, y=130, height=200, width=400)
         self.lbl1.config(fg='#000000', font=('宋体', 20))
         text = self.entryWidget.get().strip()
         text2 = self.entryW.get().strip()
@@ -75,3 +85,19 @@ class ClocFrame:
     def change(self):
         self.frame2.destroy()
         calen_gui.CanlenFrame(self.master)
+
+    def check(self):
+        self.lbl1.destroy()
+        self.text = Text(self.frame2)
+        self.text.place(x=10, y=130, height=200, width=400)
+        self.text.delete('0.0', 'end')
+        d = self.entry2.get()
+        e = self.entry3.get()
+        c.execute(sql2, (d, e))
+        a = c.fetchall()
+        header = '事件内容 事件发生时间 事件耗费时间'.split()
+        pt = PrettyTable()
+        pt._set_field_names(header)
+        for b in a:
+            pt.add_row(b)
+        self.text.insert(INSERT, pt)
